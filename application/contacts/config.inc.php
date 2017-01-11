45d3f6c300952889c97b4ce6d3ff5da204157987
robyfofo <robymant@tiscali.it>
2017-01-11 10:41:15 +0100

    Aggiornamento 11 ter gennaio

---
+++ application/contacts/config.inc.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Framework siti html-PHP-Mysql
+ * PHP Version 7
+ * @author Roberto Mantovani (<me@robertomantovani.vr.it>
+ * @copyright 2009 Roberto Mantovani
+ * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
+ * admin/contacts/config.inc.php v.3.0.0. 11/01/2017
+*/
+
+$App->params = new stdClass();
+
+/* prende i dati del modulo */
+Sql::initQuery(DB_TABLE_PREFIX.'site_modules',array('help_small','help'),array('contacts'),'name = ?');
+$obj = Sql::getRecord();
+if (Core::$resultOp->error == 0 && is_object($obj)) $App->params = $obj;
+
+$App->params->codeVersion = ' 3.0.0.';
+$App->params->pageTitle = 'Contatti';
+$App->params->breadcrumb = '<li class="active"><i class="icon-user"></i> Contatti</li>';
+
+$App->params->tables = array();
+$App->params->fields = array();
+$App->params->uploadDirs = array();
+$App->params->uploadPaths = array();
+$App->params->ordersType = array();
+$App->params->labels = array();
+
+/* ITEMS */
+$App->params->labels['item'] = array('item'=>'contatto','itemSex'=>'o','items'=>'contatti','itemsSex'=>'i','owner'=>'','ownerSex'=>'','owners'=>'','ownersSex'=>'');
+$App->params->ordersType['item'] = 'DESC';
+$App->params->tables['item'] = DB_TABLE_PREFIX.'contacts';
+$App->params->fields['item'] = array(
+	'id'=>array('label'=>'ID','required'=>false,'type'=>'autoinc','primary'=>true),
+	'name'=>array('label'=>'Titolo','searchTable'=>true,'required'=>true,'type'=>'varchar'),
+	'created'=>array('label'=>'Creazione','searchTable'=>false,'required'=>false,'type'=>'datatime'),
+	'active'=>array('label'=>'Attiva','required'=>false,'type'=>'int','defValue'=>0)
+	);
+?>
