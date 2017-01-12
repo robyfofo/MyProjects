4fc3a8e40278861a8d33ff39919cd1b0d3c4154d
robyfofo <robymant@tiscali.it>
2017-01-12 10:21:18 +0100

    Aggiornamento 12 gennaio

--- application/del-projects/items.js
+++ application/del-projects/items.js
@@ -1,5 +1,4 @@
-/* admin/contacts/items.js v.3.0.0. 11/01/2017 */
-
+/* items/application.js v.2.6.2. 17/02/2016 */
 $(document).ready(function() {
 	$('#applicationForm')
 	.bootstrapValidator({
@@ -26,7 +25,10 @@ $(document).ready(function() {
 					.addClass(isValidTab ? 'fa-check' : 'fa-times');
 					}
 			}
-		});		
+		});
+		
+	
+		
 	});
 
 	
@@ -38,24 +40,20 @@ $(document).on('focusin', function(e) {
 	
 tinymce.init({
     selector: ".editorHTML",
-    browser_spellcheck: true,
     theme: "modern",
     height: 300,
     language: "it",
-    content_style: ".mce-content-body {font-size:15px;font-family:Arial,sans-serif;}",
     plugins: [
-         "advlist autolink link image lists charmap print preview hr anchor pagebreak",
+         "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
          "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
          "save table contextmenu directionality emoticons template paste textcolor responsivefilemanager"
    ],
-	relative_urls: false,
-	remove_script_host: false,
-	convert_urls: true,
-	document_base_url: siteUrl,
+	relative_urls: true,
+	document_base_url: window.baseUrl,
 	filemanager_title:"Responsive Filemanager",
-	external_filemanager_path: siteUrl+"/filemanager/",
-	external_plugins: { "filemanager" : siteUrl+"/filemanager/plugin.min.js"},
-	content_css: siteTemplateUrl+"assets/plugins/tinymce/css/content.css",
+	external_filemanager_path: window.baseUrl+"/filemanager/",
+	external_plugins: { "filemanager" : window.baseUrl+"/filemanager/plugin.min.js"},
+	content_css: window.baseTemplateUrl+"tinymce/css/content.css",
 	image_advtab: true,
 	toolbar: " undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image responsivefilemanager | print preview media fullpage | forecolor backcolor emoticons",   
    style_formats: [
--- application/projects/items.php
+++ application/projects/items.php
@@ -1,51 +1,52 @@
 <?php
-/*
-	framework siti html-PHP-Mysql
-	copyright 2011 Roberto Mantovani
-	http://www.robertomantovani.vr;it
-	email: me@robertomantovani.vr.it
-	news/items.php v.2.6.2. 16/02/2016
+/**
+ * Framework siti html-PHP-Mysql
+ * PHP Version 7
+ * @author Roberto Mantovani (<me@robertomantovani.vr.it>
+ * @copyright 2009 Roberto Mantovani
+ * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
+ * admin/contacts/items.php v.3.0.0. 11/01/2017
 */
 
-if (isset($_POST['itemsforpage'])) $_MY_SESSION_VARS = $my_session->addSessionsModuleSingleVar($_MY_SESSION_VARS,$Tpl->subAction,'ifp',$_POST['itemsforpage']);
-if (isset($_POST['searchFromTable'])) $_MY_SESSION_VARS = $my_session->addSessionsModuleSingleVar($_MY_SESSION_VARS,$Tpl->subAction,'srcTab',$_POST['searchFromTable']);
+if (isset($_POST['itemsforpage'])) $_MY_SESSION_VARS = $my_session->addSessionsModuleSingleVar($_MY_SESSION_VARS,$App->sessionName,'ifp',$_POST['itemsforpage']);
+if (isset($_POST['searchFromTable'])) $_MY_SESSION_VARS = $my_session->addSessionsModuleSingleVar($_MY_SESSION_VARS,$App->sessionName,'srcTab',$_POST['searchFromTable']);
 
-$appData->id_cat = 0;
+$App->id_cat = 0;
 
 switch(Core::$request->method) {
 	case 'activeItem':
 	case 'disactiveItem':
-		Sql::manageFieldActive(substr($Tpl->method,0,-4),$appTableItem,$Tpl->id,ucfirst($appData->labels['item']['item']),$appData->labels['item']['itemSex']);
-		$appData->viewMethod = 'list';
+		Sql::manageFieldActive(substr(Core::$request->method,0,-4),$App->params->tables['item'],$App->id,ucfirst($App->params->labels['item']['item']),$App->params->labels['item']['itemSex']);
+		$App->viewMethod = 'list';
 	break;
 	
 	case 'deleteItem':
-		if ($Tpl->id > 0) {
-			$appData->itemOld = new stdClass;
-				Sql::initQuery($appTableItem,array('id'),array($Tpl->id),'id = ?');
+		if ($App->id > 0) {
+			$App->itemOld = new stdClass;
+				Sql::initQuery($App->params->tables['item'],array('id'),array($App->id),'id = ?');
 				Sql::deleteRecord();
 				if (Core::$resultOp->error == 0) {
-					Core::$resultOp->message = ucfirst($appData->labels['item']['item']).' cancellat'.$appData->labels['item']['itemSex'].'!';		
+					Core::$resultOp->message = ucfirst($App->params->labels['item']['item']).' cancellat'.$App->params->labels['item']['itemSex'].'!';		
 					}
 				
 			}		
-		$appData->viewMethod = 'list';
+		$App->viewMethod = 'list';
 	break;
 	
 	case 'newItem':
-		$Tpl->pageSubTitle = 'inserisci '.$appData->labels['item']['item'];
-		$appData->viewMethod = 'formNew';	
+		$Tpl->pageSubTitle = 'inserisci '.$App->params->labels['item']['item'];
+		$App->viewMethod = 'formNew';	
 	break;
 	
 	case 'insertItem':
 		if ($_POST) {	
 			if (!isset($_POST['active'])) $_POST['active'] = 0;
-			if (!isset($_POST['created'])) $_POST['created'] = $appData->nowDateTime;		   	
+			if (!isset($_POST['created'])) $_POST['created'] = $App->nowDateTime;		   	
 			/* controlla i campi obbligatori */
-			Sql::checkRequireFields($fieldsItem);
+			Sql::checkRequireFields($App->params->fields['item']);
 			if (Core::$resultOp->error == 0) {
 				Sql::stripMagicFields($_POST);
-				Sql::insertRawlyPost($fieldsItem,$appTableItem);
+				Sql::insertRawlyPost($App->params->fields['item'],$App->params->tables['item']);
 				if (Core::$resultOp->error == 0) {
 		   		}
 				}
@@ -53,29 +54,29 @@ switch(Core::$request->method) {
 				Core::$resultOp->error = 1;
 				}			
 		if (Core::$resultOp->error == 1) {
-			$Tpl->pageSubTitle = 'inserisci '.$appData->labels['item']['item'];
-			$appData->viewMethod = 'formNew';
+			$Tpl->pageSubTitle = 'inserisci '.$App->params->labels['item']['item'];
+			$App->viewMethod = 'formNew';
 			} else {
-				$appData->viewMethod = 'list';
-				Core::$resultOp->message = ucfirst($appData->labels['item']['item']).' inserit'.$appData->labels['item']['itemSex'].'!';				
+				$App->viewMethod = 'list';
+				Core::$resultOp->message = ucfirst($App->params->labels['item']['item']).' inserit'.$App->params->labels['item']['itemSex'].'!';				
 				}		
 	break;
 
 	case 'modifyItem':				
-		$Tpl->pageSubTitle = 'modifica '.$appData->labels['item']['item'];
-		$appData->viewMethod = 'formMod';
+		$Tpl->pageSubTitle = 'modifica '.$App->params->labels['item']['item'];
+		$App->viewMethod = 'formMod';
 	break;
 	
 	case 'updateItem':
 		if ($_POST) {
-			if (!isset($_POST['created'])) $_POST['created'] = $appData->nowDateTime;
+			if (!isset($_POST['created'])) $_POST['created'] = $App->nowDateTime;
 			if (!isset($_POST['active'])) $_POST['active'] = 0;
 	   	
 			/* controlla i campi obbligatori */
-			Sql::checkRequireFields($fieldsItem);
+			Sql::checkRequireFields($App->params->fields['item']);
 			if (Core::$resultOp->error == 0) {
 				Sql::stripMagicFields($_POST);
-				Sql::updateRawlyPost($fieldsItem,$appTableItem,'id',$Tpl->id);
+				Sql::updateRawlyPost($App->params->fields['item'],$App->params->tables['item'],'id',$App->id);
 				if(Core::$resultOp->error == 0) {
 			   	}					
 				}
@@ -83,91 +84,91 @@ switch(Core::$request->method) {
 				Core::$resultOp->error = 1;
 				}			
 		if (Core::$resultOp->error == 1) {
-			$Tpl->pageSubTitle = 'modifica '.$appData->labels['item']['item'];
-			$appData->viewMethod = 'formMod';				
+			$Tpl->pageSubTitle = 'modifica '.$App->params->labels['item']['item'];
+			$App->viewMethod = 'formMod';				
 			} else {
 				if (isset($_POST['submitForm'])) {	
-					$appData->viewMethod = 'list';
-					Core::$resultOp->message = ucfirst($appData->labels['item']['item']).' modificat'.$appData->labels['item']['itemSex'].'!';								
+					$App->viewMethod = 'list';
+					Core::$resultOp->message = ucfirst($App->params->labels['item']['item']).' modificat'.$App->params->labels['item']['itemSex'].'!';								
 					} else {						
 						if (isset($_POST['id'])) {
-							$Tpl->id = $_POST['id'];
-							$Tpl->pageSubTitle = 'modifica '.$appData->labels['item']['item'];
-							$appData->viewMethod = 'formMod';	
+							$App->id = $_POST['id'];
+							$Tpl->pageSubTitle = 'modifica '.$App->params->labels['item']['item'];
+							$App->viewMethod = 'formMod';	
 							Core::$resultOp->message = "Modifiche applicate!";
 							} else {
-								$appData->viewMethod = 'formNew';	
-								echo $Tpl->pageSubTitle = 'inserisci '.$appData->labels['item']['item'];
+								$App->viewMethod = 'formNew';	
+								echo $Tpl->pageSubTitle = 'inserisci '.$App->params->labels['item']['item'];
 								}
 						}				
 				}		
 	break;
 	
 	case 'pageItem':
-		$_MY_SESSION_VARS = $my_session->addSessionsModuleSingleVar($_MY_SESSION_VARS,$Tpl->subAction,'page',$Tpl->id);
-		$appData->viewMethod = 'list';	
+		$_MY_SESSION_VARS = $my_session->addSessionsModuleSingleVar($_MY_SESSION_VARS,$App->sessionName,'page',$App->id);
+		$App->viewMethod = 'list';	
 	break;
 
 	case 'messageItem':
-		Core::$resultOp->error = $Tpl->id;
+		Core::$resultOp->error = $App->id;
 		Core::$resultOp->message = urldecode(Core::$request->params[0]);
-		$appData->viewMethod = 'list';
+		$App->viewMethod = 'list';
 	break;
 
 	case 'listItem':
-		$appData->viewMethod = 'list';		
+		$App->viewMethod = 'list';		
 	break;
 
 	default;	
-		$appData->viewMethod = 'list';	
+		$App->viewMethod = 'list';	
 	break;	
 	}
 
 
 /* SEZIONE SWITCH VISUALIZZAZIONE TEMPLATE (LIST, FORM, ECC) */
 
-switch((string)$appData->viewMethod) {
+switch((string)$App->viewMethod) {
 	case 'formNew':
-		$appData->item = new stdClass;		
-		$appData->item->active = 1;
-		$appData->item->created = $appData->nowDateTime;
-		if (Core::$resultOp->error > 0) Utilities::setItemDataObjWithPost($appData->item,$fieldsItem);
-		$appData->templatePage = 'formItem.tpl.php';
-		$appData->methodForm = 'insertItem';
+		$App->item = new stdClass;		
+		$App->item->active = 1;
+		$App->item->created = $App->nowDateTime;
+		if (Core::$resultOp->error > 0) Utilities::setItemDataObjWithPost($App->item,$App->params->fields['item']);
+		$App->templatePage = 'formItem.tpl.php';
+		$App->methodForm = 'insertItem';
 	break;
 	
 	case 'formMod':
-		$appData->item = new stdClass;
-		Sql::initQuery($appTableItem,array('*'),array($Tpl->id),'id = ?');
-		$appData->item = Sql::getRecord();		
-		if (Core::$resultOp->error == 1) Utilities::setItemDataObjWithPost($appData->item,$fieldsItem);
-		$appData->templatePage = 'formItem.tpl.php';
-		$appData->methodForm = 'updateItem';	
+		$App->item = new stdClass;
+		Sql::initQuery($App->params->tables['item'],array('*'),array($App->id),'id = ?');
+		$App->item = Sql::getRecord();		
+		if (Core::$resultOp->error == 1) Utilities::setItemDataObjWithPost($App->item,$App->params->fields['item']);
+		$App->templatePage = 'formItem.tpl.php';
+		$App->methodForm = 'updateItem';	
 	break;
 
 	case 'list':
-		$appData->items = new stdClass;			
-		$appData->itemsForPage = (isset($_MY_SESSION_VARS[$Tpl->subAction]['ifp']) ? $_MY_SESSION_VARS[$Tpl->subAction]['ifp'] : 5);
-		$appData->page = (isset($_MY_SESSION_VARS[$Tpl->subAction]['page']) ? $_MY_SESSION_VARS[$Tpl->subAction]['page'] : 1);				
+		$App->items = new stdClass;			
+		$App->itemsForPage = (isset($_MY_SESSION_VARS[$App->sessionName]['ifp']) ? $_MY_SESSION_VARS[$App->sessionName]['ifp'] : 5);
+		$App->page = (isset($_MY_SESSION_VARS[$App->sessionName]['page']) ? $_MY_SESSION_VARS[$App->sessionName]['page'] : 1);				
 		$qryFields = array('*');
 		$qryFieldsValues = array();
 		$qryFieldsValuesClause = array();
 		$clause = '';
-		if (isset($_MY_SESSION_VARS[$Tpl->subAction]['srcTab']) && $_MY_SESSION_VARS[$Tpl->subAction]['srcTab'] != '') {
-			list($sessClause,$qryFieldsValuesClause) = Sql::getClauseVarsFromAppSession($_MY_SESSION_VARS[$Tpl->subAction]['srcTab'],$fieldsItem,'');
+		if (isset($_MY_SESSION_VARS[$App->sessionName]['srcTab']) && $_MY_SESSION_VARS[$App->sessionName]['srcTab'] != '') {
+			list($sessClause,$qryFieldsValuesClause) = Sql::getClauseVarsFromAppSession($_MY_SESSION_VARS[$App->sessionName]['srcTab'],$App->params->fields['item'],'');
 			}		
 		if (isset($sessClause) && $sessClause != '') $clause .= $sessClause;
 		if (is_array($qryFieldsValuesClause) && count($qryFieldsValuesClause) > 0) {
 			$qryFieldsValues = array_merge($qryFieldsValues,$qryFieldsValuesClause);	
 			}
-		Sql::initQuery($appTableItem,$qryFields,$qryFieldsValues,$clause);
-		Sql::setItemsForPage($appData->itemsForPage);	
-		Sql::setPage($appData->page);		
+		Sql::initQuery($App->params->tables['item'],$qryFields,$qryFieldsValues,$clause);
+		Sql::setItemsForPage($App->itemsForPage);	
+		Sql::setPage($App->page);		
 		Sql::setResultPaged(true);
-		if (Core::$resultOp->error <> 1) $appData->items = Sql::getRecords();
-		$appData->pagination = Utilities::getPagination($appData->page,Sql::getTotalsItems(),$appData->itemsForPage);
-		$Tpl->pageSubTitle = 'lista delle '.$appData->labels['item']['items'];
-		$appData->templatePage = 'listItem.tpl.php';	
+		if (Core::$resultOp->error <> 1) $App->items = Sql::getRecords();
+		$App->pagination = Utilities::getPagination($App->page,Sql::getTotalsItems(),$App->itemsForPage);
+		$Tpl->pageSubTitle = 'lista dei '.$App->params->labels['item']['items'];
+		$App->templatePage = 'listItem.tpl.php';	
 	break;	
 	
 	default:
